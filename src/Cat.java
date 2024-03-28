14c7815f9475854ed21960f9be478d2de4c19284
Ivanna185 <ivanna185@gmail.com>
2024-03-28 23:04:41 +0200

    task for lesson 1

---
+++ src/Cat.java
@@ -0,0 +1,31 @@
+public class Cat {
+    public static void main(String[] args) {
+
+
+
+        // int a;
+
+        int a = 7 ;
+        int b = 9;
+        System.out.println("STEP 1");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+       a =  a + b;
+       b = a - b;
+       a = a - b;
+
+
+
+        System.out.println("result");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+    }
+
+
+
+
+}
